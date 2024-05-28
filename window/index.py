5037ce041f055636e0fd90058a43e544539eb9ff
joannachou <joanna131025@gmail.com>
2024-05-28 14:44:34 +0800

    modified0244

--- window/index.py
+++ window/index.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 def get_names() -> list[str]:
     with open('names.txt',encoding='utf-8') as file:
@@ -6,8 +7,18 @@ def get_names() -> list[str]:
     names:list[str] = content.split()
     return names
 
+class Window(tk.Tk):
+    def __init__(self,title:str="Hello! Tkinter!",**kwargs):
+        super().__init__(**kwargs)
+        #多做一些事
+        self.title(title)
+        label:ttk.Label = ttk.Label(self,
+                                    text="Hello! World!",
+                                    font=('Arial',20,'bold'),
+                                    foreground='#f00')
+        label.pack(padx=100,pady=40)
+
 if __name__ == '__main__':
     names:list[str] = get_names()
-    window:tk.Tk = tk.Tk()
-    window.title('我的第一個GUI程式')
+    window:Window = Window(title="這是的第一個GUI程式") 
     window.mainloop()
