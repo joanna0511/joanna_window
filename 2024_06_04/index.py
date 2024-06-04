8558536e8aa83b39cff80b1b46bd2c83fd59454f
joannachou <joanna131025@gmail.com>
2024-06-04 14:38:51 +0800

    m

--- 2024_06_04/index.py
+++ 2024_06_04/index.py
@@ -2,12 +2,22 @@
 from pprint import pprint
 import tkinter as tk
 from tkinter import ttk
+from ttkthemes import ThemedTk
 import tools
 
-class Window(tk.Tk):
+class Window(ThemedTk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        self.title("AQI顯示")
+        self.title("全台空氣品質指標(AQI)")
+        #self.option_add("*Font","微軟正黑體 40")
+        #定義style的名稱
+        style = ttk.Style()
+        style.configure('Top.TFrame',background='#BEC23F')
+        style.configure('Top.TLabel',font=('Helvetica',25))
+
+        title_frame = ttk.Frame(self,style='Top.TFrame')
+        ttk.Label(title_frame,text='全台空氣品質指標(AQI)',style='Top.TLabel').pack()
+        title_frame.pack(padx=100,pady=50)
 
 
 def main():
@@ -20,7 +30,7 @@ def main():
         data:list[dict] = tools.get_data(all_data)
         pprint(data)
     '''
-    window = Window()
+    window = Window(theme="arc")
     window.mainloop()
     
 
