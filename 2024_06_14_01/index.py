7e3a412c67fce0b533e40e046e7d95717734925e
joanna2024 <joanna131025@gmail.com>
2024-06-14 10:28:51 +0800

    modified0614

--- 2024_06_14_01/index.py
+++ 2024_06_14_01/index.py
@@ -6,10 +6,46 @@ import data
 class Window(ThemedTk):
     def __init__(self,theme:str='arc',**kwargs):
         super().__init__(theme=theme,**kwargs)
+        self.title('台北市YouBike2.0及時資料')
         try:
             self.__data = data.load_data()
         except Exception as e:
             messagebox.showwarning(title='警告',message=str(e))
+        
+        self._display_interface()
+        
+    def _display_interface(self):
+        mainFrame = ttk.Frame(borderwidth=1,relief='groove')
+        ttk.Label(mainFrame,text="台北市YouBike2.0及時資料",font=('arial',25)).pack()
+        #=================================
+        tableFrame = ttk.Frame(mainFrame)
+        columns = ('sna', 'sarea', 'mday','ar','total','rent_bikes','retuen_bikes')
+        tree = ttk.Treeview(tableFrame, columns=columns, show='headings')
+        # define headings
+        tree.heading('sna', text='站點')
+        tree.heading('sarea', text='行政區')
+        tree.heading('mday', text='時間')
+        tree.heading('ar', text='地址')
+        tree.heading('total', text='總數')
+        tree.heading('rent_bikes', text='可借')
+        tree.heading('retuen_bikes', text='可還')
+
+        
+        
+        # add data to the treeview
+        for site in self.data:
+            tree.insert('', tk.END,
+                        values=(site['sna'],site['sarea'],site['mday'],site['ar'],site['total'],site['rent_bikes'],site['retuen_bikes']))
+        
+        tree.grid(row=0, column=0, sticky='nsew')
+
+        scrollbar = ttk.Scrollbar(tableFrame, orient=tk.VERTICAL, command=tree.yview)
+        tree.configure(yscroll=scrollbar.set)
+        scrollbar.grid(row=0, column=1, sticky='ns')
+        tableFrame.pack(expand=True,fill=tk.BOTH)
+        #======================================
+        mainFrame.pack(expand=True,fill=tk.BOTH,padx=10,pady=10)
+
 
 
     @property
