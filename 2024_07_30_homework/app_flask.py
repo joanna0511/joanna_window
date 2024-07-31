2d75d8e285f28508c7645cd8cc92d133b30602a5
joanna2024 <joanna131025@gmail.com>
2024-08-01 04:19:23 +0800

    0801

--- 2024_07_30_homework/app_flask.py
+++ 2024_07_30_homework/app_flask.py
@@ -1,13 +1,19 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask App!"
 
 @app.route('/api/single_plot', methods=['POST'])
 def single_plot():
     data = request.json
-    # 處理數據邏輯
+    return jsonify({"message": "成功接收到數據", "data": data})
+
+@app.route('/api/multi_plot', methods=['POST'])
+def multi_plot():
+    data = request.json
     return jsonify({"message": "成功接收到數據", "data": data})
 
 if __name__ == '__main__':
